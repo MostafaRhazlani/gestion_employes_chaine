669c11c026a69a624e0244b92d0d3f4132497b11
MostafaRhazlani <mo.rhazlani@gmail.com>
2023-03-17 16:17:49 +0100

    create page register and login and logout in views and create page user in models and create page userController in controller

---
+++ models/user.php
@@ -0,0 +1,39 @@
+<?php
+
+  class user {
+
+    static public function login($data) {
+      $username = $data['username'];
+
+      try {
+          $query = 'SELECT * FROM users WHERE username = :username';
+          $stmt = DB::connect()->prepare($query);
+          $stmt->execute(array(":username" => $username));
+          $user = $stmt->fetch(PDO::FETCH_OBJ);
+          return $user;
+          if($stmt->execute()) {
+            return 'ok';
+          }
+      } catch(PDOException $ex) {
+          echo 'Erreur' . $ex->getMessage();
+      }
+    }
+
+    static public function createUser($data) {
+      $stmt = DB::connect()->prepare('INSERT INTO users (fullname, username, password) VALUES
+      (:fullname, :username, :password)');
+      $stmt->bindParam(':fullname',$data['fullname']);
+      $stmt->bindParam(':username',$data['username']);
+      $stmt->bindParam(':password',$data['password']);
+
+      if($stmt->execute($data)){
+        return 'ok';
+      } else {
+        return 'error';
+      }
+      // $stmt->close();
+      $stmt = null;
+    }
+  }
+
+?>
